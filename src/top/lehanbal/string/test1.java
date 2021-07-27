f02c6129ba641a429ed9cf8b92d0994034e13751
lehanbal <tcr980726@gmail.com>
2021-07-27 09:29:59 -0700

    add algs libs

--- src/top/lehanbal/string/test1.java
+++ src/top/lehanbal/string/test1.java
@@ -1,7 +1,9 @@
 package top.lehanbal.string;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class test1 {
     public static void main(String[] args) {
-
+        StdOut.printf("%d", 12);
     }
 }
