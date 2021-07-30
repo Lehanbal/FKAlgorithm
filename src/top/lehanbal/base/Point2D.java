8c3c3982289d5a95d4db29a175d88bc5c337d596
lehanbal <tcr980726@gmail.com>
2021-07-30 07:44:50 -0700

    update Point2D

---
+++ src/top/lehanbal/base/Point2D.java
@@ -0,0 +1,141 @@
+package top.lehanbal.base;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Point2D implements Comparable<Point2D>{
+    private final double x;
+    private final double y;
+
+    public Point2D(double x, double y) {
+        if (Double.isInfinite(x) || Double.isInfinite(y))
+            throw new IllegalArgumentException("Coordinates must be finite");
+        if (Double.isNaN(x) || Double.isNaN(y))
+            throw new IllegalArgumentException("Coordinates cannot be NaN");
+
+        if(x == 0.0) this.x = 0.0;
+        else this.x = x;
+
+        if(y == 0.0) this.y = 0.0;
+        else this.y = y;
+    }
+
+    public double X(){
+        return x;
+    }
+
+    public double Y(){
+        return y;
+    }
+
+    public double r(){
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Comparator<Point2D> polarOrder() {
+        return new PolarOrder();
+    }
+
+    private class PolarOrder implements Comparator<Point2D> {
+        public int compare(Point2D q1, Point2D q2) {
+            double dx1 = q1.x - x;
+            double dy1 = q1.y - y;
+            double dx2 = q2.x - x;
+            double dy2 = q2.y - y;
+
+            if      (dy1 >= 0 && dy2 < 0) return -1;    // q1 above; q2 below
+            else if (dy2 >= 0 && dy1 < 0) return +1;    // q1 below; q2 above
+            else if (dy1 == 0 && dy2 == 0) {            // 3-collinear and horizontal
+                if      (dx1 >= 0 && dx2 < 0) return -1;
+                else if (dx2 >= 0 && dx1 < 0) return +1;
+                else                          return  0;
+            }
+            else return -ccw(Point2D.this, q1, q2);     // both above or below
+
+            // Note: ccw() recomputes dx1, dy1, dx2, and dy2
+        }
+    }
+
+    public static int ccw(Point2D a, Point2D b, Point2D c) {
+        double area2 = (b.x-a.x)*(c.y-a.y) - (b.y-a.y)*(c.x-a.x);
+        if      (area2 < 0) return -1;
+        else if (area2 > 0) return +1;
+        else                return  0;
+    }
+
+    @Override
+    public int compareTo(Point2D that) {
+        if (this.y < that.y) return -1;
+        if (this.y > that.y) return +1;
+        if (this.x < that.x) return -1;
+        if (this.x > that.x) return +1;
+        return 0;
+    }
+
+
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    public void drawTo(Point2D that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    @Override
+    public int hashCode() {
+        int hashX = ((Double) x).hashCode();
+        int hashY = ((Double) y).hashCode();
+        return 31*hashX + hashY;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Point2D that = (Point2D) other;
+        return this.x == that.x && this.y == that.y;
+    }
+
+    public static void main(String[] args) {
+        int x0 = Integer.parseInt(args[0]);
+        int y0 = Integer.parseInt(args[1]);
+        int n = Integer.parseInt(args[2]);
+
+
+        StdDraw.setCanvasSize(800, 800);
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(0, 100);
+        StdDraw.setPenRadius(0.005);
+        StdDraw.enableDoubleBuffering();
+
+        Point2D[] points = new Point2D[n];
+        for (int i = 0; i < n; i++) {
+            int x = StdRandom.uniform(100);
+            int y = StdRandom.uniform(100);
+            points[i] = new Point2D(x, y);
+            points[i].draw();
+        }
+
+        Point2D p = new Point2D(x0, y0);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius(0.02);
+        p.draw();
+
+        StdDraw.setPenRadius();
+        StdDraw.setPenColor(StdDraw.BLUE);
+        Arrays.sort(points, p.polarOrder());
+        for (int i = 0; i < n; i++) {
+            p.drawTo(points[i]);
+            StdDraw.show();
+            StdDraw.pause(100);
+        }
+
+        Bag
+
+    }
+}
